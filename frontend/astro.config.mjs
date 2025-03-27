dc7a7e309dfa9cc0145ca2be1d53c39d4616c409
Nikhil Patel <68939870+naive-nikhil@users.noreply.github.com>
2025-03-27 11:54:58 +0530

    Some Changes

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://naive-nikhil.github.io/birthday',
     vite: {
         plugins: [tailwindcss()],
       },
